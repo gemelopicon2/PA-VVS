067e21294e13b91ecd515f3c0fbaaf251be90078
saulrapos <saul.raposo@udc.es>
2026-05-07 16:56:08 +0200

    [FUNC-3] Visualizar información detallada de una sesión

--- frontend/src/backend/catalogService.js
+++ frontend/src/backend/catalogService.js
@@ -2,3 +2,15 @@ import {appFetch} from './appFetch';
 
 export const getBillboard = (date) =>
     appFetch('GET', `/catalog/movies?date=${date}`);
+
+export const findMovieById = (id) =>
+    appFetch('GET', `/catalog/movies/${id}`);
+
+export const findSessionById = (id) =>
+    appFetch('GET', `/catalog/sessions/${id}`);
+
+export const findMovieById = (id) =>
+    appFetch('GET', `/catalog/movies/${id}`);
+
+export const findSessionById = (id) =>
+    appFetch('GET', `/catalog/sessions/${id}`);
